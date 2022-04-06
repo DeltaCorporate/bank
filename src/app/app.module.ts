0ca6bc51a84c981fb26224df7054e5ce2e797e71
DeltaCorporate <houms937@gmail.com>
2022-04-06 10:11:00 +0200

    creationg bottom tab bar with good style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BottomTabComponent } from './components/nav/bottom-tab/bottom-tab.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,BottomTabComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", "./src/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
